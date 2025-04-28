36e163850c5ba649294600e55a7e9df10c181114
Paulo Hericles <paulo.neves.rgr@segurosunimed.com.br>
2025-04-27 21:18:24 -0300

    chore: add config in vscode file settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { HistoryComponent } from './pages/history/history.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'historico', component: HistoryComponent },
-];
+export const routes: Routes = [{ path: '', component: AppComponent }];
